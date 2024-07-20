06bf49c7ce3dca137a74bbb8e3862a0ed25d2fbf
Singidi Sai Naga Sudheer <123714207+Sudheer625@users.noreply.github.com>
2024-07-20 20:15:59 +0530

    Add files via upload

---
+++ typeconversion.js
@@ -0,0 +1,17 @@
+/*
+type conversion -
+changing one data type to another datatype
+*/
+
+let age = 25 
+age = Number (age);
+
+age += 1;
+
+console.log(age, typeof age);
+
+/*
+Number - for integers 
+String - for strings 
+Boolean - for bool's
+*/
